98af5e27896d5d28b98a1ec33ee25818c53e60bb
Aurora Dess Hartman <gh0st13@gh0stp4wz.is-a.dev>
2026-01-27 11:43:19 +0100

    Package Stuff required for compile

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "Virtual-Boy-v2",
+		author: "gh0stp4wz",
+		version: "0.1-dev",
+		description: "A retro aestetic theme inspired by a failed console of the same name, Now Rewritten from Scratch!",
+		source: "https://github.com/gh0stp4wz/Virtual-Boy-v2"
+	}
+}
