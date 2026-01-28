3579d5be451c0d97f78e9d3229288558376c2fd6
Aurora Dess Hartman <gh0st13@gh0stp4wz.is-a.dev>
2026-01-28 13:58:35 +0100

    Bump internal version

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -3,7 +3,7 @@ export default {
 	meta: {
 		name: "Virtual-Boy-v2",
 		author: "nexus-themes",
-		version: "0.1-dev",
+		version: "0.2-dev",
 		description: "A retro aestetic theme inspired by a failed console of the same name, Now Rewritten from Scratch!",
 		source: "https://github.com/gh0stp4wz/Virtual-Boy-v2"
 	}
